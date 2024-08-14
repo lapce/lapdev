d5ceede1cffa97d17d013827622a00ae3a70502a
Lu Yang <ly@lap.dev>
2024-08-14 22:30:56 +0100

    add logs folder config

--- lapdev-api/src/server.rs
+++ lapdev-api/src/server.rs
@@ -35,12 +35,25 @@ struct Cli {
     /// The config file path
     #[clap(short, long, action, value_hint = clap::ValueHint::AnyPath)]
     config_file: Option<PathBuf>,
+    /// The folder for putting logs
+    #[clap(short, long, action, value_hint = clap::ValueHint::AnyPath)]
+    logs_folder: Option<PathBuf>,
 }
 
-pub async fn run(additional_router: Option<Router<CoreState>>) -> Result<()> {
+pub async fn start(additional_router: Option<Router<CoreState>>) {
     let cli = Cli::parse();
+
+    let _result = setup_log(&cli).await;
+
+    if let Err(e) = run(&cli, additional_router).await {
+        tracing::error!("lapdev api start server error: {e:#}");
+    }
+}
+
+async fn run(cli: &Cli, additional_router: Option<Router<CoreState>>) -> Result<()> {
     let config_file = cli
         .config_file
+        .clone()
         .unwrap_or_else(|| PathBuf::from("/etc/lapdev.conf"));
     let config_content = tokio::fs::read_to_string(&config_file)
         .await
@@ -144,4 +157,36 @@ pub async fn run(additional_router: Option<Router<CoreState>>) -> Result<()> {
             }
         });
     }
+}
+
+async fn setup_log(
+    cli: &Cli,
+) -> Result<tracing_appender::non_blocking::WorkerGuard, anyhow::Error> {
+    let folder = cli
+        .logs_folder
+        .clone()
+        .unwrap_or_else(|| PathBuf::from("/var/lib/lapdev/logs"));
+    tokio::fs::create_dir_all(&folder).await?;
+    let file_appender = tracing_appender::rolling::Builder::new()
+        .max_log_files(30)
+        .rotation(tracing_appender::rolling::Rotation::DAILY)
+        .filename_prefix("lapdev.log")
+        .build(folder)?;
+    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+    let filter = tracing_subscriber::EnvFilter::default()
+        .add_directive("lapdev=info".parse()?)
+        .add_directive("lapdev_api=info".parse()?)
+        .add_directive("lapdev_conductor=info".parse()?)
+        .add_directive("lapdev_rpc=info".parse()?)
+        .add_directive("lapdev_common=info".parse()?)
+        .add_directive("lapdev_db=info".parse()?)
+        .add_directive("lapdev_enterprise=info".parse()?)
+        .add_directive("lapdev_proxy_ssh=info".parse()?)
+        .add_directive("lapdev_proxy_http=info".parse()?);
+    tracing_subscriber::fmt()
+        .with_ansi(false)
+        .with_env_filter(filter)
+        .with_writer(non_blocking)
+        .init();
+    Ok(guard)
 }
